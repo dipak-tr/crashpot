274bd8672e23f84af4a173ce59c52ec709cd36cf
Dipak Patel <dipak.p@tridhya.com>
2020-03-06 12:20:11 +0530

    adding dashboard API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 use Validator;
