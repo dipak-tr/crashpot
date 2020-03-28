be44d536934fd58fbdff94b946a79a81e6b6510a
Dipak Patel <dipakptridhyatech@gmail.com>
2020-03-28 16:53:58 +0530

    firebase push notitfication

--- app/Http/Controllers/Voyager/AdminNotificationsController.php
+++ app/Http/Controllers/Voyager/AdminNotificationsController.php
@@ -33,7 +33,7 @@ class AdminNotificationsController extends BaseVoyagerBaseController {
     //
     //****************************************
 
-    public function index(Request $request) {
+    public function index(Request $request) { 
         // GET THE SLUG, ex. 'posts', 'pages', etc.
         $slug = $this->getSlug($request);
 
@@ -403,7 +403,7 @@ class AdminNotificationsController extends BaseVoyagerBaseController {
                 ->get();
 
         foreach ($userLists as $userList) {
-
+          
             if ($userList->id == 28) {
                 $avata = url('/') . '/images/users/default.png';
 
@@ -906,32 +906,44 @@ class AdminNotificationsController extends BaseVoyagerBaseController {
         //return true;
         //$API_ACCESS_KEY = config('constants.fcm_app_server_key');
         $API_ACCESS_KEY = setting('site.fcm_app_server_key');
-        /*         * ******************************************************************* */
-        $url = "https://fcm.googleapis.com/fcm/send";
-        $token = "dMH7OE7YSlmlZf86cyUstZ:APA91bFgrFlmxyETk82Ek7CKdGPiq8ZRbmvMQG6lS8n25Ghyzf3KicmRdu6YopGg5_qrwgGoNue4oep7G2QamtRoQtmCHVgJ7DtDPEYXS5KmB0ezn9c84_YDsa1oSJmcUWP4mT9SMNoj";
-        $serverKey = $API_ACCESS_KEY;
-        $title = "Title";
-        $body = "Body of the message";
-        $notification = array('title' => $title, 'text' => $body, 'sound' => 'default', 'badge' => '1');
-        $arrayToSend = array('to' => $token, 'notification' => $notification, 'priority' => 'high');
-        $json = json_encode($arrayToSend);
-        $headers = array();
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'Authorization: key=' . $serverKey;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
+       /**********************************************************************/
+        define( 'API_ACCESS_KEY', $API_ACCESS_KEY ); // get API access 
 
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//Send the request
-        $response = curl_exec($ch);
-//Close request
-        if ($response === FALSE) {
-            die('FCM Send Error: ' . curl_error($ch));
-        }
-        curl_close($ch);
-        /*         * ******************************************************************* */
+$registrationIds = array( 'dMH7OE7YSlmlZf86cyUstZ:APA91bFgrFlmxyETk82Ek7CKdGPiq8ZRbmvMQG6lS8n25Ghyzf3KicmRdu6YopGg5_qrwgGoNue4oep7G2QamtRoQtmCHVgJ7DtDPEYXS5KmB0ezn9c84_YDsa1oSJmcUWP4mT9SMNoj' ); //Replace this with your device token
+
+
+// Modify custom payload here
+$msg = array
+(
+        'mesgTitle'     => 'SMART TESTING',
+        'alert'         => 'This is sample notification'
+
+);
+$fields = array
+(
+    'registration_ids'      => $registrationIds,
+    'data'                  => $msg
+);
+
+$headers = array
+(
+    'Authorization: key=' . API_ACCESS_KEY,
+    'Content-Type: application/json'
+);
+
+$ch = curl_init();
+curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' ); //For firebase, use https://fcm.googleapis.com/fcm/send
+
+curl_setopt( $ch,CURLOPT_POST, true );
+curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+$result = curl_exec($ch );
+curl_close( $ch );
+echo $result;
+
+       /**********************************************************************/
         $responceData = array();
         $notification = array();
         if (!empty($type)) {
@@ -963,7 +975,7 @@ class AdminNotificationsController extends BaseVoyagerBaseController {
             $fields['notification'] = $notification;
             \Log::info($notification);
         }
-
+       
         try {
             $ch = curl_init();
             // curl_setopt($ch, CURLOPT_URL, config('constants.fcm_push_notification_url'));
